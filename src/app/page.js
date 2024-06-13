b11a8302e9dc53f606a765def1447deece17ac94
carey0001 <2212788301@qq.com>
2024-06-13 11:13:28 +0800

    修改：准备资源，正i页面结构和文案

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,53 @@
-import Image from "next/image";
-
 export default function Home() {
+  // tag数组
+  const tagArr = [
+    "COLORS",
+    "VARIABLES",
+    "SHADOWS",
+    "TYPOGRAPHY",
+    "GRIDS",
+    "FORMS PLUGIN",
+    "BACKDROP BLUR",
+    "BLURS",
+    "SPACING",
+    "BORDERS",
+    "OPACITY",
+    "CURSORS"
+  ];
   return (
-    <main className="">
-      项目初始化
+    <main>
+      <div
+        style={{
+          backgroundImage: "url(/bg-sky.png)"
+        }}
+      >
+        <div>
+          <div>NEW: SCOPED Typography Variables 🥳</div>
+        </div>
+
+        <div>
+          <div>
+            <img src="/logo.svg" alt="tailwindcss-logo" />
+            <div>tailwindcss</div>
+          </div>
+          <div>
+            <div>UNOFFICIAL DESIGN SYSTEM</div>
+            <div>v3.4.3</div>
+          </div>
+          <div>by @praveenjuge</div>
+        </div>
+
+        <div>
+          {tagArr.map((tag, idx) => {
+            return (
+              <div key={idx}>
+                <img src="/tw-logo.svg" width={32} alt="tailwindcss-logo" />
+                <span>{tag}</span>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,29 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  // prefix: 'tw-',
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      screens: {
+        'xs': '375px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
+      fontSize: {
+        'xs': ['0.75rem', { lineHeight: '1rem' }],
+        'sm': ['0.875rem', { lineHeight: '1.25rem' }],
+        'md': ['1rem', { lineHeight: '1.5rem' }],
+        'lg': ['1.125rem', { lineHeight: '1.75rem' }],
+        'xl': ['1.25rem', { lineHeight: '1.75rem' }],
+        '2xl': ['1.5rem', { lineHeight: '2rem' }],
+        '3xl': ['1.75rem', { lineHeight: '2rem' }],
+        '4xl': ['2rem', { lineHeight: '2.25rem' }],
       },
     },
   },
   plugins: [],
+  corePlugins: {}
 };
