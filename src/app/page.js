e37c988f3d71f5f7682c4a21f1ca2fe39c58fdb0
carey0001 <2212788301@qq.com>
2024-06-13 12:00:40 +0800

    修改：更新tw配置；整理页面样式

--- src/app/page.js
+++ src/app/page.js
@@ -15,36 +15,36 @@ export default function Home() {
     "CURSORS"
   ];
   return (
-    <main>
-      <div
-        style={{
-          backgroundImage: "url(/bg-sky.png)"
-        }}
-      >
-        <div>
-          <div>NEW: SCOPED Typography Variables 🥳</div>
+    <main className="w-full flex justify-center pt-8 px-4 text-center font-medium">
+      <div className="w-full max-w-screen-xl shadow-xl border rounded-2xl" style={{
+        backgroundImage: 'url(/bg-sky.png)'
+      }}>
+        <div className="pt-10 pb-6">
+          <div className="inline-block py-3 md:py-4 px-6 md:px-8 text-sm lg:text-2xl md:text-lg sm:text-md xs:text-sm text-sky-500 rounded-full bg-sky-500/10 border-2 border-sky-300">
+            NEW: SCOPED Typography Variables 🥳
+          </div>
         </div>
 
-        <div>
-          <div>
-            <img src="/logo.svg" alt="tailwindcss-logo" />
-            <div>tailwindcss</div>
+        <div className="py-4 lg:py-6">
+          <div className="flex justify-center align-middle gap-2 lg:gap-4">
+            <img src="/tw-logo.svg" alt="tailwindcss-logo" className="w-[3rem] lg:w-[4.5rem] md:w-[4rem] sm:w-[3rem]" />
+            <div className=" text-4xl lg:text-6xl md:text-6xl sm:text-5xl xs:text-5xl text-slate-950">tailwindcss</div>
           </div>
-          <div>
-            <div>UNOFFICIAL DESIGN SYSTEM</div>
-            <div>v3.4.3</div>
+          <div className="py-6 text-xs lg:text-3xl md:text-2xl sm:text-xl xs:text-lg">
+            <div className="text-sky-500 pb-1">UNOFFICIAL DESIGN SYSTEM</div>
+            <div className="text-slate-950">v3.4.3</div>
           </div>
-          <div>by @praveenjuge</div>
+          <div className="text-sky-500 pb-6 text-xs lg:text-xl md:text-md sm:text-md xs:text-sm">by @praveenjuge</div>
         </div>
 
-        <div>
+        <div className="max-w-screen-md m-auto pb-[2rem] flex justify-center flex-wrap">
           {tagArr.map((tag, idx) => {
             return (
-              <div key={idx}>
-                <img src="/tw-logo.svg" width={32} alt="tailwindcss-logo" />
-                <span>{tag}</span>
+              <div key={idx} className="flex gap-1 lg:gap-4 md:gap-2 px-4 py-1 m-2 rounded-full bg-sky-500/10 border border-sky-300">
+                <img src="/tw-logo.svg" alt="tailwindcss-logo" className="w-[16px] lg:w-[20px] md:w-[18px] sm:w-[16px]" />
+                <span className="text-sky-500 text-xs lg:text-xl md:text-md sm:text-md xs:text-sm">{tag}</span>
               </div>
-            );
+            )
           })}
         </div>
       </div>
